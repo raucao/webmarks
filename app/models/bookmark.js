012fce8a31899622ccc0b3ad560731eb442070b0
Sebastian Kippe <sebastian@kip.pe>
2013-11-07 12:58:22 +0000

    Fix timestamp usage

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -12,6 +12,10 @@ var Bookmark = Ember.Object.extend({
     return moment(this.get('createdAt')).fromNow();
   }.property('createdAt'),
 
+  updatedAtTimeAgo: function() {
+    return moment(this.get('updatedAt')).fromNow();
+  }.property('updatedAt'),
+
   domain: function() {
     var a = document.createElement('a');
     a.href = this.get('url');
@@ -21,6 +25,13 @@ var Bookmark = Ember.Object.extend({
   serialize: function() {
     var serialized = this.getProperties('url', 'title', 'description');
     var tags = this.get('tags');
+
+    var createdAt = this.get('createdAt');
+    if (createdAt !== null) {
+      console.log(typeof createdAt);
+      serialized.createdAt = createdAt;
+    }
+
     if (tags.length > 0) {
       serialized.tags = tags.split(',').map($.trim);
     }
