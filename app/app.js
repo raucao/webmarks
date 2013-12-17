24a4c60768750d52a1011495a7552c00b95aa1ca
Sebastian Kippe <sebastian@kip.pe>
2013-12-17 20:25:39 +0100

    Make Ember be less chatty by default

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@ import registerComponents from 'appkit/utils/register_components';
 import Bookmark from 'appkit/models/bookmark';
 
 var App = Ember.Application.extend({
-  LOG_ACTIVE_GENERATION: true,
-  LOG_MODULE_RESOLVER: true,
-  LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
+  // LOG_ACTIVE_GENERATION: true,
+  // LOG_MODULE_RESOLVER: true,
+  // LOG_TRANSITIONS: true,
+  // LOG_TRANSITIONS_INTERNAL: true,
+  // LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'appkit', // TODO: loaded via config
   Resolver: Resolver,
   archiveBookmarks: [],
