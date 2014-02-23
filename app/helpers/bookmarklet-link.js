64f25e7956fca49bcc78a98d1e6296ef6e7df4e5
Sebastian Kippe <sebastian@kip.pe>
2014-02-23 19:52:46 +0400

    Buttons

--- app/helpers/bookmarklet-link.js
+++ app/helpers/bookmarklet-link.js
@@ -1,5 +1,5 @@
 var bookmarkletLink = Ember.Handlebars.makeBoundHelper(function() {
-  return new Handlebars.SafeString("<a href=\"javascript:void(location.href='"+window.location.origin+"/bookmarks/new/?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title))\" class=\"pure-button\">Create Webmark</a>");
+  return new Handlebars.SafeString("<a href=\"javascript:void(location.href='"+window.location.origin+"/bookmarks/new/?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title))\" class=\"button\">Create Webmark</a>");
 });
 
 export default bookmarkletLink;
