5dc5fe8102cda706538ef59fecf73aea3a04fb24
Garret Alfert <alfert@wevelop.de>
2016-12-11 02:10:36 +0100

    Edit a clone of the bookmark instead of original (fixes #14)
    Otherwise all edits would change the local data even when cancelling.

--- app/routes/bookmarks/edit.js
+++ app/routes/bookmarks/edit.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import Bookmark from 'webmarks/models/bookmark';
 
 export default Ember.Route.extend({
+
+  storage: Ember.inject.service(),
+
+  setupController(controller, model) {
+    // clone the bookmark for editing so it's only persisted when
+    // submitting the form
+    this._super(controller, Bookmark.create(model));
+  }
+
 });
