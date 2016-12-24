f6b9e2055a1e48d07453d9fbdd481f8c1de7eb3e
Sebastian Kippe <sebastian@kip.pe>
2016-12-24 16:38:24 +0800

    Add tests for bookmark form autofocus

---
+++ tests/integration/components/bookmark-form/component-test.js
@@ -0,0 +1,23 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('bookmark-form', 'Integration | Component | bookmark form', {
+  integration: true
+});
+
+test('it sets focus on the title field when empty', function(assert) {
+  this.set('bookmark', {title: null});
+  this.render(hbs`{{bookmark-form bookmark=bookmark}}`);
+
+  assert.equal(this.$('input#title').val(), '');
+  assert.equal(this.$('input#title').is(":focus"), true);
+});
+
+test('does not set focus on the title field when filled', function(assert) {
+  this.set('bookmark', {title: 'foo bar'});
+  this.render(hbs`{{bookmark-form bookmark=bookmark}}`);
+
+  assert.equal(this.get('bookmark.title'), 'foo bar');
+  assert.equal(this.$('input#title').val(), 'foo bar');
+  assert.equal(this.$('input#title').is(":focus"), false);
+});
