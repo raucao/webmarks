3571a74fea629c1d7bfdd268dfea9f4fe53f37e8
Garret Alfert <alfert@wevelop.de>
2017-02-04 23:11:27 +0100

    Move search field focusing into component

--- app/components/bookmark-search/component.js
+++ app/components/bookmark-search/component.js
@@ -5,8 +5,24 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['search-input'],
 
-  autofocusSearchField: function() {
-    this.$('input[type=text]').focus();
-  }.on('didInsertElement')
+  focusSearchField() {
+    if (!document.invisible) {
+      Ember.run.scheduleOnce('afterRender', () => {
+        this.$('input[type=text]')[0].focus();
+      });
+    }
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.focusSearchField();
+    this.set('visibilityHandler', this.focusSearchField.bind(this));
+    document.addEventListener("visibilitychange", this.get('visibilityHandler'), false);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    document.removeEventListener("visibilitychange", this.get('visibilityHandler'));
+  }
 
 });
