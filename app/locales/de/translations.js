016edbb37af7a805864a397f7119ded5183f849e
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-07-17 17:35:36 +0200

    Localize bookmark edit page

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -11,6 +11,17 @@ export default {
       updated: 'aktualisiert',
       you_have_not_stored_any_bookmarks_yet: 'Sie haben noch keine Lesezeichen gespeichert.',
       add_the_first_one: 'Füge das erste hinzu'
+    },
+    edit: {
+      header: 'Bookmark aktualisieren',
+      form: {
+        title: 'Titel',
+        url: 'URL',
+        description: 'Beschreibung',
+        tags: 'Tags',
+        save: 'Versenden',
+        cancel: 'Abbrechen'
+      }
     }
   },
   links: {
--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -2,15 +2,26 @@
 
 export default {
   welcome_page: {
-      welcome_to_webmarks: 'Welcome to Webmarks!',
-      please_connect_your_remote_storage_first: 'Please connect your remote storage first.',
-      upcoming_relase_note: '(This will not be necessary for the upcoming release version of Webmarks.)'
+    welcome_to_webmarks: 'Welcome to Webmarks!',
+    please_connect_your_remote_storage_first: 'Please connect your remote storage first.',
+    upcoming_relase_note: '(This will not be necessary for the upcoming release version of Webmarks.)'
   },
   bookmark: {
     index: {
       updated: 'updated',
       you_have_not_stored_any_bookmarks_yet: 'You haven&#39;t stored any bookmarks yet.',
       add_the_first_one: 'Add the first one'
+    },
+    edit: {
+      header: 'Update bookmark',
+      form: {
+        title: 'Title',
+        url: 'URL',
+        description: 'Description',
+        tags: 'Tags',
+        save: 'Save',
+        cancel: 'Cancel'
+      }
     }
   },
   links: {
