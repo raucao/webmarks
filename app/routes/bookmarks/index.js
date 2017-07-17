826c2913c2f22efcb6cfe24aba88468b25283294
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-07-17 14:44:09 +0200

    Add configuration for localization
    refs #12
    install ember-i18n
    add localization for en and de
    set default languange to en
    add translation for search input field place holder

--- app/routes/bookmarks/index.js
+++ app/routes/bookmarks/index.js
@@ -4,6 +4,12 @@ import RequireRSConnection from 'webmarks/mixins/require-rs-connection';
 export default Ember.Route.extend(RequireRSConnection, {
 
   storage: Ember.inject.service(),
+  i18n: Ember.inject.service(),
+
+  afterModel: function(user) {
+    console.log(user);
+    // this.set('i18n.locale', user.get('locale'));
+  },
 
   model() {
     return this.get('storage').getBookmarks();
