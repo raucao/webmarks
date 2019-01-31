0d6671aa153a680d7c9fba58f0fc18b0498b869c
Sebastian Kippe <sebastian@kip.pe>
2019-01-31 17:05:45 +0700

    Add SW in prod, cache app assets
    refs #61

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,13 @@ module.exports = function(defaults) {
         'icon_x128.png',
         'icon_x144.png',
         'icon_x256.png'
-      ]
+      ],
+    },
+    'asset-cache': {
+      include: [
+        'assets/**/*',
+        '*.png'
+      ],
     }
   });
 
