8899232cc5cc623e4fbe3e0beea3fee75441cf2e
Hugo Roy <hugo@hugoroy.eu>
2019-02-05 14:46:48 +0700

    Edit French translation
    https://github.com/skddc/webmarks/issues/73#issuecomment-460358324

--- app/locales/fr/translations.js
+++ app/locales/fr/translations.js
@@ -7,16 +7,16 @@ export default {
       bookmarklet: {
         use_text: 'Utiliser le bookmarklet',
         drag_text: 'Glisser ce lien vers votre barre de favoris:',
-        create_webmark: 'Créer Webmark'
+        create_webmark: 'Créer le Webmark'
       }
     },
     index: {
       updated: 'modifié',
-      you_have_not_stored_any_bookmarks_yet: 'Tu n&#39;as pas encore sauvé de favoris',
+      you_have_not_stored_any_bookmarks_yet: 'Tu n\'as pas encore enregistré de favoris',
       add_the_first_one: 'Ajouter le premier'
     },
     edit: {
-      header: 'Modifier favori',
+      header: 'Modifier le favori',
       form: {
         title: 'Titre',
         url: 'URL',
